410c93611d2a928a053c00ada39c43956546b1fe
Junior <jmota.wk@hotmail.com>
2021-04-13 20:10:16 -0300

    create: BusinessPerson migration

---
+++ database/migrations/2021_04_13_200639_create_business_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessPeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('position')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_people');
+    }
+}
