99871a7767c051f0640822cd3f075c0041db74d5
Junior <jmota.wk@hotmail.com>
2021-04-13 21:12:46 -0300

    create: BusinessManagementRole migration

---
+++ app/Models/BusinessManagementRole.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BusinessManagementRole extends Model
+{
+    use HasFactory;
+}
